edd7bd49d0e1eab155282cbef8452034f208b1ed
ป๋อม ลุงปั่น <123305588+pomlungpun@users.noreply.github.com>
2025-12-15 19:10:38 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+    public Book() {
+        this("Unknown","Unknown",1.0 );
+    }
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("author: " + author);
+        System.out.println("price: " + price);
+        System.out.println("---------------------------------------------------------------------------------------------");
+
+    }
+    public void updatePrice(double newPrice) {
+        if(newPrice<0){
+            System.out.println("---------------------------------------impossible---------------------------------------");
+            this.price = 1.0;
+        }
+        else{
+            this.price = newPrice;
+        }
+
+    }
+    public void applyDiscount(double Discount){
+        if(Discount>=0.00 && Discount<=100.00){
+            double discountAmount = this.price*(Discount/100);
+            double newPrice = this.price - discountAmount;
+            this.price = newPrice;
+        }
+        else {
+            System.out.println("---------------------------------------Error discount---------------------------------------");
+        }
+
+    }
+}
---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+      public static void main(String[] args){
+          Book unknownBook = new Book();
+          unknownBook.displayDetails();
+
+          Book hp1Book = new Book("Harry Potter and the Philosopher\'s Stone", "J.K. Rowling", 350.00);
+          hp1Book.displayDetails();
+
+          hp1Book.updatePrice(100);
+          hp1Book.displayDetails();
+
+          hp1Book.applyDiscount(150);
+          hp1Book.displayDetails();
+
+
+
+
+  }
+}
